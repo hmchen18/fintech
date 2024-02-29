83dc19d24429b4e4c2fc642ae1a9495b9f5581cc
hmchen18 <161006100+hmchen18@users.noreply.github.com>
2024-02-29 21:58:19 +0800

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,8 +1,4 @@
-# A first-level heading
-## A second-level heading
-### A third-level heading
-```ruby
-require 'redcarpet'
-markdown = Redcarpet.new("Hello World!")
-puts markdown.to_html
+
+```
+print("Hello", "how are you?")
 ```
